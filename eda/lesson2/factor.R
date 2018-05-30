8e9cf1f33d765fed12b89cc3cfd30ceee78ae657
lckof-mac <suyu0925@gmail.com>
2018-05-30 16:42:24 +0800

    factor

--- eda/lesson2/factor.R
+++ eda/lesson2/factor.R
@@ -1,14 +1,14 @@
 setwd('./lesson2')
 reddit = read.csv('reddit.csv')
 
-# str(reddit)
+str(reddit)
 
 # table(reddit$employment.status)
+summary(reddit)
 
-# summary(reddit$employment.status)
+levels(reddit$age.range)
+reddit$age.range.order <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "25-34",
+                                                               "35-44", "45-54", "55-64", "65 or Above"))
 
-reddit$employment.status
-
-install.packages('ggplot2')
 library('ggplot2')
-
+qplot(data = reddit, x = age.range.order)
